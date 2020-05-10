d744a94a2b3eab12d4c3aae7220e852040560466
suman151 <sumangoel151@gmail.com>
2020-05-10 14:05:26 +0530

    Added more questions

---
+++ Array/bitwise_operators.py
@@ -0,0 +1,19 @@
+a = 10
+b = 4
+#bitwise and operation
+print("a & b = ", a & b)
+
+#bitwise or operator
+print("a | b = ", a | b)
+
+#bitwise xor operator
+print("a ^ b = ", a ^ b)
+
+#bitwise not operator
+print("~a = ",~a)
+
+#right shift operator
+print("b>> = ",b>>1)
+
+#lsft shift operators
+print("b<< = ",b<<1)
