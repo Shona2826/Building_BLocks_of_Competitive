6c832a4cb3c6e23cd09737d9c32d2c32300eb0e0
suman151 <sumangoel151@gmail.com>
2020-05-23 21:42:22 +0530

    Added LinkedList Problems

---
+++ LinkedList/begining_cycle.py
@@ -0,0 +1,10 @@
+def has_cycle(head):
+	slow=fast = head
+	while fast and fast.next and fast.next.next:
+		slow,fast = slow.next,fast.next.next
+		if slow is fast:
+			slow = head
+			while slow is not fast:
+				slow,fast = slow.next, fast.next
+			return slow
+		return None
