983d1d4d89d54c6732ab009d6492beaa50a0b0fa
suman151 <sumangoel151@gmail.com>
2020-06-23 15:38:21 +0530

    Added

---
+++ Array/buy.py
@@ -0,0 +1,16 @@
+a = [100,180,260,310,40,535,695]
+countbuy = 0
+countsell = 0
+for i in range(len(a)):
+	if countsell = 0:
+		if a[i] < a[i+1]:
+		buy = a[i]
+		countbuy = 1
+	if countbuy = 1:
+		countbuy = 0
+		for j in range(i+1,len(a)):
+			if a[j] > a[j+1]:
+			sell = a[j]
+			countsell = 1
+		countsel= 0
+	
