f9881f79d37093631d05879a36f9865bdd68e49d
suman151 <sumangoel151@gmail.com>
2020-07-28 22:26:04 +0530

    Added

---
+++ Leetcode July Challenge/all_paths_from_source_to_target.py
@@ -0,0 +1,15 @@
+class Solution:
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        visited = [False for i in range(len(graph))]
+        res = []
+        def dfs(node,ans):
+            visited[node] = True
+            if node == len(graph)-1:
+                res.append(ans[:])
+                return 
+            for child in graph[node]:
+                ans.append(child)
+                dfs(child,ans)
+                ans.pop()
+        dfs(0,[0])
+        return res
